8dfbb404b1da1eed6809be78c7229275906cc4f8
Dante Dupont <dmxdupont@gmail.com>
2025-06-05 19:36:02 -0400

    new game button working

--- src/components/GameLobby.tsx
+++ src/components/GameLobby.tsx
@@ -1,17 +1,25 @@
-import { Link, useLoaderData } from "react-router"
+import { Link, useLoaderData, useNavigate } from "react-router"
 import type { Game } from "../game/game"
 import { useState } from "react"
+import { TicTacToeApiClient } from '../api'
+
+const api = new TicTacToeApiClient
 
 const GameLobby = () => {
     const { games: initialGames } = useLoaderData<{ games: Game[]}>()
-
     const [games, setGames] = useState<Game[]>(initialGames)
+    const navigate = useNavigate()
+
+    async function handleNewGame() {
+        const newGame = await api.createGame()
+        setGames([...games, newGame])
+        navigate(`/game/${newGame.id}`)
+    }
 
     return(
         <div>
             <h2 className="text-xl">GameLobby</h2>
-            <button>Start New Game</button>
-            Super Cool List of Games
+            <button onClick={handleNewGame}>Start New Game</button>
             {games.map(game => (
                 <div key={game.id}>
                         <Link to={`/game/${game.id}`}>Game: {game.id.slice(-2)}</Link>
